1bd9a11ff0a8fe33473833bd8ae30294a9ae1622
Simon Willison <swillison@gmail.com>
2020-11-24 16:13:42 -0800

    Fixed datasette.client bug introd in last commit
    Refs https://github.com/simonw/datasette/issues/1102

--- datasette-{{cookiecutter.hyphenated}}/tests/test_{{cookiecutter.underscored}}.py
+++ datasette-{{cookiecutter.hyphenated}}/tests/test_{{cookiecutter.underscored}}.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.mark.asyncio
 async def test_plugin_is_installed():
     datasette = Datasette([], memory=True)
-    response = await client.get("/-/plugins.json")
+    response = await datasette.client.get("/-/plugins.json")
     assert 200 == response.status_code
     installed_plugins = {p["name"] for p in response.json()}
     assert "datasette-{{ cookiecutter.hyphenated }}" in installed_plugins
